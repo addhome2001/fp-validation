91ff968d660ba8cba5b17e2b7fad27274a41b58e
seven <seven@megalive.com.tw>
2016-09-22 22:55:31 +0800

    Added unit test

--- test.js
+++ test.js
@@ -1,30 +1,45 @@
 'use strict';
 
 const tape = require('tape');
-const vail = require('./dist');
+const validate = require('./lib').default;
+const libs = require('./libs');
 
-const lib = vail.lib;
-const validate = vail.default;
+const msg = "Custom Message";
+
+const singleLib = lib => {
+  const v = validate(lib());
+  const vCostom = validate(lib(msg));
+  return { v, vCostom }
+}
 
 tape('isRequired', t => {
-  const v = validate(lib.isRequired());
-  const msg = "Custom Message";
-  const vCostom = validate(lib.isRequired(msg));
-  t.is(v(""), "欄位為必填", 'Must return default reason');
-  t.is(vCostom(""), msg, 'Must return custom reason');
-
-  t.false(v("Text"), "Should pass if value is not Empty");
+  const vail = singleLib(libs.isRequired);
+  t.is(vail.v(""), "欄位為必填", 'Must return default reason when is empty');
+  t.is(vail.vCostom(""), msg, 'Must return custom reason');
+  t.false(vail.v("Text"), "Should pass if value is not Empty");
   t.end();
 });
 
 tape('isNumber', t => {
-  const v = validate(lib.isNumber());
-  const msg = "Custom Message";
-  const vCostom = validate(lib.isRequired(msg));
-  t.is(v(""), "輸入內容必須為數字", 'Must return default reason');
-  t.is(vCostom(""), msg, 'Must return custom reason');
+  const vail = singleLib(libs.isNumber);
+  t.is(vail.v(""), "輸入內容必須為數字", 'Must return default reason when not a number');
+  t.is(vail.vCostom(""), msg, 'Must return custom reason');
+  t.false(vail.v(1), "Should pass if value is not Empty")
+  t.end();
+});
 
-  t.ok(v("Text"), 'Must return reason when not a number');
-  t.false(v(1), "Should pass if value is not Empty")
+tape('isEmail', t => {
+  const vail = singleLib(libs.isEmail);
+  t.is(vail.v("no email"), "電子信箱格式不正確", 'Must return default reason when not a email');
+  t.is(vail.vCostom("no email"), msg, 'Must return custom reason');
+  t.false(vail.v("abe@xxx.com"), "Should pass if value is Email")
+  t.end();
+});
+
+tape('isCellphone', t => {
+  const vail = singleLib(libs.isCellphone);
+  t.is(vail.v("12345678"), "手機號碼格式不正確", 'Must return default reason when not a cellphone');
+  t.is(vail.vCostom("12345678"), msg, 'Must return custom reason');
+  t.false(vail.v("0912345678"), "Should pass if value is cellphone");
   t.end();
 });
