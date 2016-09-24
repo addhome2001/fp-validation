289787dfdde2af62bb9e6a7fa97b3789cc975984
seven <seven@megalive.com.tw>
2016-09-24 16:59:22 +0800

    Modified format.js logic

--- src/utils/format.js
+++ src/utils/format.js
@@ -3,10 +3,10 @@ export default function (condition) {
     const args = Array.prototype.splice.call(arguments, 0);
     const singleArgu = args.length <= 1;
     const [head] = args;
-    const msg = singleArgu ? (typeof head === 'string' && head) || "" : args.pop();
+    const msg = singleArgu ? (typeof head === 'string' && head) : args.pop();
     return function({ reason, val }) {
       if (!reason) {
-        const cond = singleArgu ? [val] : args.concat(val);
+        const cond = singleArgu && msg ? [val] : args.concat(val);
         const { con, msg: resultMsg } = condition.apply(this, cond);
         return con ?
         { reason: null, val } :
